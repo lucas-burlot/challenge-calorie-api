f8aae2e7c1ec4f9e3b9adef1abefd958528617fd
Pewoh <burl410@gmail.com>
2023-03-15 11:15:59 +0100

    Add apidoc + user doc

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// Global variables
+const apidoc = require('apidoc');
 require('dotenv').config();
 const app = express();
 const recipeRoute = require('./routes/recipe.route.js');
 const userRoute = require('./routes/user.route.js');
-const ingredientRoute = require('./routes/ingredient.route.js');
+const ingredientRoute = require('./routes/ingredient.route.js')
+const path = require("path");
+
 // load body parser
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(bodyParser.json());
+app.use('/apidoc', express.static(path.join(__dirname, 'apidoc')));
 app.use(userRoute);
 app.use(recipeRoute);
 app.use(ingredientRoute);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-
-// Login and get JWT token
+/**
+ * @api {post} /login Login
+ * @apiName Login
+ * @apiGroup Users
+ *
+ * @apiParam {String} username User's username.
+ * @apiParam {String} password User's password.
+ *
+ * @apiSuccess {String} token JWT token to authenticate user.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "message": "Success"
+ *       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+ *     }
+ *
+ * @apiError {String} message Error message.
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad request
+ *     {
+ *       "message": "Username and password are required"
+ *     }
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 401 Unauthorized
+ *     {
+ *       "message": "Invalid credentials"
+ *     }
+ */
 router.post('/login', userController.login);
-// Register (username, password)
+
+/**
+ * @api {post} /register Register
+ * @apiName Register
+ * @apiGroup Users
+ *
+ * @apiParam {String} username User's username.
+ * @apiParam {String} password User's password.
+ *
+ * @apiSuccess {String} message Registration success.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *       "message": "User created"
+ *     }
+ *
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad request
+ *     {
+ *       "message": "Username and password are required"
+ *     }
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad request
+ *     {
+ *       "message": "Username already exist"
+ *     }
+ */
 router.post('/register', userController.register);
 
 module.exports = router;
