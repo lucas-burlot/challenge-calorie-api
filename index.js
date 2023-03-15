9372169d13931e784e6aef06f1749e2599a959b1
Pewoh <burl410@gmail.com>
2023-03-15 14:40:34 +0100

    MAJ doc

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const apidoc = require('apidoc');
 require('dotenv').config();
 const app = express();
 const recipeRoute = require('./routes/recipe.route.js');
